c904ac88acb40dd737b0c2ab35b17eeac7da55b5
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-04-11 23:31:36 -0400

    init commit

---
+++ model/model.js
@@ -0,0 +1,55 @@
+var homeContent = `<div class="hero">
+<p class="hero-title">Aerocell</p>
+<p class="hero-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam tempora exercitationem beatae vel. Ut tempore hic doloribus doloremque, exercitationem cumque ad asperiores, error obcaecati fuga perspiciatis laborum enim repudiandae voluptate.</p>
+</div>
+
+<div class="featured-vid">
+<p class="featured-title">Featured Video</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OOQDbvPxoQo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>`;
+
+var aboutContent = `<div class="about">
+<p class="about-title">about</p>
+<div class="about-container">
+    <div class="about-image"></div>
+    <p class="about-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni animi atque quaerat inventore expedita! Non, tempora. Deleniti eligendi quis, dicta odio repellendus architecto ad ea dolorem sequi saepe accusantium magnam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni animi atque quaerat inventore expedita! Non, tempora. Deleniti eligendi quis, dicta odio repellendus architecto ad ea dolorem sequi saepe accusantium magnam!</p>
+</div>
+
+</div>`;
+
+var discographyContent = `<div class="albums">
+<p class="albums-title">albums</p>
+<div class="album-container">
+    <div class="album">
+        <div class="album-art" style="background-image: url(images/a1559954961_10.jpg)"></div>
+        <p class="album-title">Xyralon Volume 1 (2018)</p>
+    </div>
+    <div class="album">
+        <div class="album-art" style="background-image: url(images/a2201012822_10.jpg)"></div>
+        <p class="album-title">Welcome to the Void (2018)</p>
+    </div>
+</div>
+</div>
+
+<div class="recent-tracks">
+<p class="tracks-title">recent tracks</p>
+<div class="soundcloud">
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1193611432&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-986973517" title="HOJL" target="_blank" style="color: #cccccc; text-decoration: none;">HOJL</a> · <a href="https://soundcloud.com/user-986973517/hyenaedon-hojl-sleet" title="Hyenaedon &amp; HOJL - Sleet" target="_blank" style="color: #cccccc; text-decoration: none;">Hyenaedon &amp; HOJL - Sleet</a></div>
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1140892525&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-986973517" title="HOJL" target="_blank" style="color: #cccccc; text-decoration: none;">HOJL</a> · <a href="https://soundcloud.com/user-986973517/hyenaedon-hojl-challenger" title="Hyenaedon &amp; HOJL - Challenger" target="_blank" style="color: #cccccc; text-decoration: none;">Hyenaedon &amp; HOJL - Challenger</a></div>
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1018911112&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-986973517" title="HOJL" target="_blank" style="color: #cccccc; text-decoration: none;">HOJL</a> · <a href="https://soundcloud.com/user-986973517/predator-hojl-dune-acropolis" title="Predator &amp; HOJL - Dune Acropolis" target="_blank" style="color: #cccccc; text-decoration: none;">Predator &amp; HOJL - Dune Acropolis</a></div>
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/931320001&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-986973517" title="HOJL" target="_blank" style="color: #cccccc; text-decoration: none;">HOJL</a> · <a href="https://soundcloud.com/user-986973517/wyvernspawn" title="Wyvernspawn" target="_blank" style="color: #cccccc; text-decoration: none;">Wyvernspawn</a></div>
+</div>
+</div>`;
+
+var contactContent = `<div class="contact">
+<p class="contact-title">contact me</p>
+<form action="">
+    <input type="email" class="email" placeholder="Email">
+    <textarea name="message" id="message" cols="1" rows="10" placeholder="Message"></textarea>
+    <input type="submit" class="submit" >
+</form>
+</div>`;
+
+export function modelPageName (pgName){
+    $("#app").html(eval(pgName));
+}
